53d0b689924163c68ba16f56077f4099f353fcd0
renatoferreirarenatoferreira@gmail.com <renatoferreirarenatoferreira@gmail.com@33af17c4-9b61-342d-82ff-7fa920f1c477>
2012-04-15 15:19:15 +0000

    Fixed compatibility issues with Arduino 1.0.

--- trunk/AnalogEvent/AnalogEvent.h
+++ trunk/AnalogEvent/AnalogEvent.h
@@ -30,7 +30,12 @@
 #define AnalogEvent_h
 
 #include <stdlib.h>
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
 #include "WProgram.h"
+#endif
 
 struct AnalogPortInformation {
   short pin;
